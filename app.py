a75817b71c378745a6936b31a19da52ebf8244a7
T4n14J1m3n3z <153782553+T4n14J1m3n3z@users.noreply.github.com>
2024-01-06 12:27:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-$ ls -a
-.git .gitignore .streamlit app.py README.md requirements.txt
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
